94ec9b2f61b1f93f01eb3ca9cbe73c6ae2212407
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:27:52 -0600

    started

---
+++ AccessSomeNames/AccessSomeNames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccessSomeNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
