fc37b65323874cadac3c0a851940e0ec1f7c56af
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:39:58 -0600

    Update Program.cs

--- EnterInvoices/EnterInvoices/Program.cs
+++ EnterInvoices/EnterInvoices/Program.cs
@@ -16,7 +16,7 @@ namespace EnterInvoices
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new EnterInvoices());
         }
     }
 }
