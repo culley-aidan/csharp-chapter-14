4ce15680c39206056deb023d1f55e026f411b7b9
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:48:13 -0600

    Update EnterInvoices.cs

--- EnterInvoices/EnterInvoices/EnterInvoices.cs
+++ EnterInvoices/EnterInvoices/EnterInvoices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,23 @@ namespace EnterInvoices
 {
     public partial class EnterInvoices : Form
     {
+        const string DELIM = ",", FILENAME = "Invoices.txt";
+        int num;
+        string name;
+        double amount;
+        static FileStream outFile = new FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(outFile);
         public EnterInvoices()
+            => InitializeComponent();
+        private void enterButton_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            num = Convert.ToInt32(invoiceBox.Text);
+            name = nameBox.Text;
+            amount = Convert.ToDouble(amountBox.Text);
+            writer.WriteLine(num + DELIM + name + DELIM + amount);
+            invoiceBox.Clear();
+            nameBox.Clear();
+            amountBox.Clear();
         }
     }
 }
