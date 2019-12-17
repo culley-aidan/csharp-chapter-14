08dee6360a545d991f5e7454b074bba4062b5279
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:57:25 -0600

    Update ViewInvoices.cs

--- ViewInvoices/ViewInvoices/ViewInvoices.cs
+++ ViewInvoices/ViewInvoices/ViewInvoices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace ViewInvoices
 {
     public partial class ViewInvoices : Form
     {
+        const char DELIM = ',';
+        const string FILENAME = "Invoices.txt";
+        string recordIn;
+        string[] fields;
+        static FileStream file = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(file);
         public ViewInvoices()
+            => InitializeComponent();
+        private void btnRecords_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            recordIn = reader.ReadLine();
+            fields = recordIn.Split(DELIM);
+            invoiceBox.Text = fields[0];
+            nameBox.Text = fields[1];
+            amountBox.Text = fields[2];
         }
     }
 }
