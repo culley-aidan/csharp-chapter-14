4ace15a64f39b9e6224e94e0896319f79c14e07f
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:53:23 -0600

    Update ViewInvoices.Designer.cs

--- ViewInvoices/ViewInvoices/ViewInvoices.Designer.cs
+++ ViewInvoices/ViewInvoices/ViewInvoices.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.btnRecords = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(23, 24);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(123, 40);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Click the button \r\nto view records";
+            // 
+            // btnRecords
+            // 
+            this.btnRecords.Location = new System.Drawing.Point(196, 24);
+            this.btnRecords.Name = "btnRecords";
+            this.btnRecords.Size = new System.Drawing.Size(75, 40);
+            this.btnRecords.TabIndex = 1;
+            this.btnRecords.Text = "View\r\nrecords\r\n";
+            this.btnRecords.UseVisualStyleBackColor = true;
             // 
             // ViewInvoices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRecords);
+            this.Controls.Add(this.lblHeading);
             this.Name = "ViewInvoices";
             this.Text = "Invoice Data";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button btnRecords;
     }
 }
 
