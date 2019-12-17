c48ec660f5e328d838f0c6fd1d705e8decc755fc
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:40:10 -0600

    Create EnterInvoices.Designer.cs

---
+++ EnterInvoices/EnterInvoices/EnterInvoices.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace EnterInvoices
+{
+    partial class EnterInvoices
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(13, 13);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(136, 20);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Enter invoice data";
+            // 
+            // EnterInvoices
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "EnterInvoices";
+            this.Text = "nvoice Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+    }
+}
+
