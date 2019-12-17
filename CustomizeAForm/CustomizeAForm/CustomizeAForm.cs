a0553edaf66047b3609ed22cea7694f8a81cec84
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 10:22:44 -0600

    d

---
+++ CustomizeAForm/CustomizeAForm/CustomizeAForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CustomizeAForm 
+{
+    public partial class CustomizeAForm : Form 
+    {
+        public CustomizeAForm()
+            => InitializeComponent();
+        private void btnSave_Click(object sender, EventArgs e) 
+        {
+            StreamWriter file = new StreamWriter(@"..\\..\\..\\Settings.txt");
+            if (colorListBox.SelectedItem.ToString() != "" && maskedWidth.Text != "" && maskedHeight.Text != "" && txtTitle.Text != "") {
+                file.WriteLine(colorListBox.SelectedItem);
+                file.WriteLine(maskedWidth.Text + "," + maskedHeight.Text);
+                file.WriteLine(txtTitle.Text);
+                file.Close();
+                btnSave.Enabled = false;
+            }
+        }
+    }
+}
---
+++ RetrieveCustomizedForm/RetrieveCustomizedForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RetrieveCustomizedForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            var lines = File.ReadLines(@"\\FRANCISTUTTLE.EDU\Home\Student\IT\se1028304\My Documents\custom_form.txt");
+            BackColor = Color.FromName(lines.ElementAt(0));
+            int x = int.Parse(lines.ElementAt(1).Substring(0, lines.ElementAt(1).IndexOf(',')));
+            int y = int.Parse(lines.ElementAt(1).Substring(lines.ElementAt(1).IndexOf(',')+1));
+            Width = x;
+            Height = y;
+            Text = lines.ElementAt(2);
+        }
+    }
+}
