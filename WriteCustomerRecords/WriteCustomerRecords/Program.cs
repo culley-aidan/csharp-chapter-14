499b94c4ee6bef5047b016ff0f48cd1b93e6d167
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 09:36:14 -0600

    Update Program.cs

--- WriteCustomerRecords/WriteCustomerRecords/Program.cs
+++ WriteCustomerRecords/WriteCustomerRecords/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WriteCustomerRecords
 {
@@ -6,7 +7,30 @@ namespace WriteCustomerRecords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileStream file = new FileStream("..\\..\\..\\Customers.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+            Console.Write("Enter ID number: ");
+            int.TryParse(Console.ReadLine(), out int id);
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Amount Owed");
+            double.TryParse(Console.ReadLine(), out double owed);
+            Customer tmp = new Customer(id, name, owed);
+            writer.WriteLine($"{tmp.IDNumber},{tmp.Name},{tmp.AmountOwed}");
+            writer.Close();
+            file.Close();
+        }
+        class Customer
+        {
+            public int IDNumber { get; set; }
+            public string Name { get; set; }
+            public double AmountOwed { get; set; }
+            public Customer(int id, string name, double owed)
+            {
+                IDNumber = id;
+                Name = name;
+                AmountOwed = owed;
+            }
         }
     }
 }
