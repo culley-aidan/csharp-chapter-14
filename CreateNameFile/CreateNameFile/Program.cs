cfc44baf7d60d954ca21d9547f6d4e9f937bfdb3
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:20:57 -0600

    Update Program.cs

--- CreateNameFile/CreateNameFile/Program.cs
+++ CreateNameFile/CreateNameFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CreateNameFile
 {
@@ -6,7 +7,15 @@ namespace CreateNameFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileStream file = new FileStream("Names.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+            string[] names = { "Anthony", "Belle", "Carolyn", "David", "Edwin", "Frannie", "Gina", "Hannah", "Inez", "Juan" };
+            for (int x = 0; x < names.Length; ++x)
+            {
+                writer.WriteLine(names[x]);
+            }
+            writer.Close();
+            file.Close();
         }
     }
 }
