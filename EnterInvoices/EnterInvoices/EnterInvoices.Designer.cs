4ca224431d69c209612d8ece39ea7a553172b4d8
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:41:28 -0600

    Update EnterInvoices.Designer.cs

--- EnterInvoices/EnterInvoices/EnterInvoices.Designer.cs
+++ EnterInvoices/EnterInvoices/EnterInvoices.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.lblHeading = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblHeading
@@ -41,11 +44,41 @@
             this.lblHeading.TabIndex = 0;
             this.lblHeading.Text = "Enter invoice data";
             // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(14, 55);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(82, 13);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Invoice Number";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(14, 80);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Last name ";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(13, 106);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(43, 13);
+            this.lblAmount.TabIndex = 3;
+            this.lblAmount.Text = "Amount";
+            // 
             // EnterInvoices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.lblHeading);
             this.Name = "EnterInvoices";
             this.Text = "Invoice Data";
@@ -57,6 +90,9 @@
         #endregion
 
         private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAmount;
     }
 }
 
