739402f71c567a80279a8adab5399f22a7991bf6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-17 08:17:25 -0600

    started

---
+++ CreateNameFile/CreateNameFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateNameFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
