550f0b4b5c9b886fab4ac9ae4b3688b92616d1f6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-18 08:19:13 -0600

    f

---
+++ FindCustomerRecords/FindCustomerRecords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FindCustomerRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream("..\\..\\..\\..\\..\\WriteCustomerRecords\\WriteCustomerRecords\\Customers.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+
+            Console.Write("Enter ID: ");
+            string id = Console.ReadLine(), currentLine = reader.ReadLine();
+            
+            while (!string.IsNullOrEmpty(currentLine))
+            {
+                if (Regex.IsMatch(currentLine, id))
+                {
+                    Console.WriteLine($"Found customer {currentLine}");
+                    break;
+                }
+                currentLine = reader.ReadLine();
+            }
+            file.Close();
+            reader.Close();
+        }
+    }
+}
